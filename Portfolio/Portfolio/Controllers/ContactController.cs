5d1e6d67bab7756aa3e2e3f0f8dbe5460866416f
Kris Boyce <kris.boyce93@gmail.com>
2014-09-27 21:35:44 -0600

    initial commit
    Essentially complete. Some format reworking is anticipated.

---
+++ Portfolio/Portfolio/Controllers/ContactController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class ContactController : Controller
+    {
+        //
+        // GET: /Contact/
+        Models.PortfolioEntities db = new Models.PortfolioEntities();
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(new Models.ContactForm());
+        }
+
+        [HttpPost]
+        public ActionResult Index(Models.ContactForm contactForm)
+        {
+            db.ContactForms.Add(contactForm);
+            db.SaveChanges();
+            return RedirectToAction("ThankYou", "Contact");
+        }
+
+        public ActionResult ThankYou()
+        {
+            return View();
+        }
+    }
+}
---
+++ Portfolio/Portfolio/Models/ContactFormValidation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Portfolio.Models
+{
+    [MetadataType(typeof(ContactFormValidation))]
+    public partial class ContactForm
+    {
+    }
+
+    public class ContactFormValidation
+    {
+        [Required]
+        public string ContactFirstName { get; set; }
+
+        [Required]
+        public string ContactLastName { get; set; }
+        
+        [Required]
+        public string ContactEmail { get; set; }
+        
+        [Required]
+        public string ContactPhone { get; set; }
+        
+        [Required]
+        public string ContactComment { get; set; }
+
+    }
+}
